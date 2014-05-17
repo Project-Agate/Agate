b01cace18076a69ee5823c91ca415317d3195a52
David Chang <zeta11235813@gmail.com>
2014-05-17 22:04:46 +0800

    Add protocol for serialization

---
+++ Agate/STASerializableProtocol.h
@@ -0,0 +1,17 @@
+//
+//  STASerializableProtocol.h
+//  Agate
+//
+//  Created by zeta on 2014/5/15.
+//  Copyright (c) 2014年 shotdoor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol STASerializableProtocol <NSObject>
+
+@required
+
+- (NSArray*) agateSignals;
+
+@end
